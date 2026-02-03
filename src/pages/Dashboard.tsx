d04952cd9476f6921bd3bd8ade2e78a45d9f11ed
Robin MASINI <robinmasini@MacBook-Air-RM.local>
2026-02-03 14:37:38 +0100

    UI: Change Relances icon to bell

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -78,7 +78,8 @@ const Dashboard = () => {
                         onClick={() => setActiveMenu('relances')}
                     >
                         <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                            <path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z" />
+                            <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+                            <path d="M13.73 21a2 2 0 0 1-3.46 0" />
                         </svg>
                         <span>Relances</span>
                     </button>
